9cc1ec06e4c4622e596e8698cc2c5b46c7c73264
Yangon <mac@MacdeMacBook-Air.local>
2023-04-08 19:52:08 +0800

    Initial commit

---
+++ src/test/java/com/example/fromzerotoexpert/FromZerotoExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fromzerotoexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromZerotoExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
